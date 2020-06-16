f59748dbb7f2dc1233aa9904927f02f67e7f4b64
unknown <imohgenius@yahoo.com>
2020-06-16 09:42:10 +0100

    web ready

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$('#carouselExampleControls').on('slide.bs.carousel', function(e) {
+    if(e.relatedTarget.id == 'secondSlide'){
+        setTimeout(function () {
+        $("#project").css('background-color', '#3E4894').delay("slow");
+        $(".aldelia").toggleClass("d-none");
+        $(".mobile").toggleClass("d-none");
+        $(".projects").toggleClass("customcolor");
+        $(".btncarousel").toggleClass("customcolor");
+    }, 600);
+    } else if(e.relatedTarget.id == 'firstSlide'){
+        setTimeout(function () {
+            $("#project").css('background-color', 'antiquewhite').delay("slow");
+            $(".aldelia").toggleClass("d-none");
+        $(".mobile").toggleClass("d-none");
+        $(".projects").toggleClass("customcolor");
+        $(".btncarousel").toggleClass("customcolor");
+        }, 600);
+    }
+})
