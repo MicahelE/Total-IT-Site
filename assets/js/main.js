f55de19038c222b8b71d52e710ca00fe78c6894d
unknown <imohgenius@yahoo.com>
2020-07-21 14:06:33 +0100

    new site

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,3 +71,61 @@ $('#carouselExampleControls').on('slide.bs.carousel', function(e) {
         }, 600);
     }
 })
+
+
+
+$("#contact-form").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phoneno = $("#phoneno").val();
+    var projecttype = $("#projecttype").val();
+    var description = $("#description").val();
+    // var message = $("#message").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "assets/php/process.php",
+        data: "name=" + name + "&email=" + email + "&phoneno=" + phoneno + "&projecttype=" + projecttype + "&description=" + description,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+
+function submitMSG(valid, msg){
+    var msgClasses;
+if(valid){
+    msgClasses = "h3 text-center animated tada text-success";
+} else {
+    msgClasses = "h3 text-center text-danger";
+}
+$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+
+function formSuccess(){
+    submitMSG(true, "Message Submitted!")
+}
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
